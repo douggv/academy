fb5cb98660fa749f97dccf3dda46a63aec6c201c
douggv <douglasgv0502@gmail.com>
2026-01-11 20:31:16 -0400

    Create baseball.sql

---
+++ baseball.sql
@@ -0,0 +1,123 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 11-01-2026 a las 22:34:54
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `baseball`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `academias`
+--
+-- Error leyendo la estructura de la tabla baseball.academias: #1932 - Table &#039;baseball.academias&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla baseball.academias: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `baseball`.`academias`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asistencia`
+--
+-- Error leyendo la estructura de la tabla baseball.asistencia: #1932 - Table &#039;baseball.asistencia&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla baseball.asistencia: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `baseball`.`asistencia`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_asistencia_jugador`
+--
+-- Error leyendo la estructura de la tabla baseball.detalle_asistencia_jugador: #1932 - Table &#039;baseball.detalle_asistencia_jugador&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla baseball.detalle_asistencia_jugador: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `baseball`.`detalle_asistencia_jugador`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_juego_academias`
+--
+-- Error leyendo la estructura de la tabla baseball.detalle_juego_academias: #1932 - Table &#039;baseball.detalle_juego_academias&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla baseball.detalle_juego_academias: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `baseball`.`detalle_juego_academias`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `historial`
+--
+-- Error leyendo la estructura de la tabla baseball.historial: #1932 - Table &#039;baseball.historial&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla baseball.historial: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `baseball`.`historial`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `juegos`
+--
+-- Error leyendo la estructura de la tabla baseball.juegos: #1932 - Table &#039;baseball.juegos&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla baseball.juegos: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `baseball`.`juegos`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `jugadores`
+--
+-- Error leyendo la estructura de la tabla baseball.jugadores: #1932 - Table &#039;baseball.jugadores&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla baseball.jugadores: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `baseball`.`jugadores`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `objetivos`
+--
+-- Error leyendo la estructura de la tabla baseball.objetivos: #1932 - Table &#039;baseball.objetivos&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla baseball.objetivos: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `baseball`.`objetivos`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `planificacion`
+--
+-- Error leyendo la estructura de la tabla baseball.planificacion: #1932 - Table &#039;baseball.planificacion&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla baseball.planificacion: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `baseball`.`planificacion`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `progreso`
+--
+-- Error leyendo la estructura de la tabla baseball.progreso: #1932 - Table &#039;baseball.progreso&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla baseball.progreso: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `baseball`.`progreso`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+-- Error leyendo la estructura de la tabla baseball.roles: #1932 - Table &#039;baseball.roles&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla baseball.roles: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `baseball`.`roles`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+-- Error leyendo la estructura de la tabla baseball.usuarios: #1932 - Table &#039;baseball.usuarios&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla baseball.usuarios: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `baseball`.`usuarios`&#039; en la linea 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
